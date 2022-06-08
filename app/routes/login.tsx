e994b79226c063bb201f8f720acfaf449b9da4cd
Tim Kozemzak <timkozemzak@gmail.com>
2022-06-07 20:11:37 -0700

    adds login validation and redirect functionality

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,6 +1,54 @@
 import { useState } from 'react'
 import Layout from '~/components/layout'
 import FormField from '~/components/form-field'
+import { ActionFunction, json } from '@remix-run/node'
+import { validateEmail, validateName, validatePassword } from '~/utils/validators.server'
+import { login, register } from '~/utils/auth.server'
+
+export const action: ActionFunction = async ({ request }) => {
+    console.log("action is running")
+    const form = await request.formData()
+    const action = form.get('_action')
+    const email = form.get('email')
+    const password = form.get('password')
+    let firstName = form.get('firstName')
+    let lastName = form.get('lastName')
+
+    if (typeof action !== 'string' || typeof email !== 'string' || typeof password !== 'string') {
+        return json({ error: 'Invalid form data', form: action }, { status: 400 })
+    }
+
+    if (action === 'register' && (typeof firstName !== 'string' || typeof lastName !== 'string')) {
+        return json({ error: 'Invalid form data', form: action }, { status: 400 })
+    }
+
+    const errors = {
+        email: validateEmail(email),
+        password: validatePassword(password),
+        ...(action === 'register' ? {
+            firstName: validateName((firstName as string) || ''),
+            lastName: validateName((lastName as string) || ''),
+        }
+            : {})
+    }
+    if (Object.values(errors).some(Boolean)) {
+        return json({ errors, fields: { email, password, firstName, lastName }, form: action }, { status: 400 })
+    }
+
+    switch (action) {
+        case 'login': {
+            return await login({ email, password })
+        }
+        case 'register': {
+            firstName = firstName as string
+            lastName = lastName as string
+            return await register({ email, password, firstName, lastName })
+        }
+        default:
+            return json({ error: 'Invalid form data' }, { status: 400 })
+    }
+
+}
 
 const Login = () => {
     const [formData, setFormData] = useState({
--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,7 +1,35 @@
-import type { RegisterForm } from "./types.server"
+import type { RegisterForm, LoginForm } from "./types.server"
 import { prisma } from "./prisma.server"
-import { json } from "@remix-run/node"
+import { redirect, json, createCookieSessionStorage } from "@remix-run/node"
 import { createUser } from "./users.server"
+import bcrypt from 'bcryptjs';
+
+const sessionSecret = process.env.SESSION_SECRET
+if (!sessionSecret) {
+    throw new Error("SESSION_SECRET must be set")
+}
+
+const storage = createCookieSessionStorage({
+    cookie: {
+        name: 'kudos-session',
+        secure: process.env.NODE_ENV === 'production',
+        secrets: [sessionSecret],
+        sameSite: 'lax',
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30,
+        httpOnly: true,
+    }
+})
+
+export const createUserSession = async (userId: string, redirectTo: string) => {
+    const session = await storage.getSession();
+    session.set('userId', userId)
+    return redirect(redirectTo, {
+        headers: {
+            'Set-Cookie': await storage.commitSession(session),
+        }
+    })
+}
 
 export const register = async (user: RegisterForm) => {
     const exists = await prisma.user.count({ where: { email: user.email } })
@@ -14,6 +42,69 @@ export const register = async (user: RegisterForm) => {
         return json({
             error: 'Something went wrong trying to create a new user.',
             fields: { email: user.email, password: user.password},
-        }, { status: 400 })
+        }, { status: 400 }
+        )
     }
+    return createUserSession(newUser.id, '/');
 }
+
+export const login = async ({ email, password }: LoginForm) => {
+    const user = await prisma.user.findUnique({
+        where: { email },
+    })
+    console.log("user: ", user)
+
+    if(!user || !(await bcrypt.compare(password, user.password))) {
+        return json({ error: 'Incorrect login'}, { status: 400 })
+    }
+
+    return createUserSession(user.id, '/');
+}
+
+export const requireUserId = async (request:Request, redirectTo: string = new URL(request.url).pathname) => {
+    const session = await getUserSession(request)
+    const userId = session.get('userId')
+    if (!userId || typeof userId !== 'string') {
+        const searchParams = new URLSearchParams([['redirectTo', redirectTo]])
+        throw redirect(`/login?${searchParams}`)
+    }
+    return userId
+}
+
+const getUserSession = (request:Request) => {
+    return storage.getSession(request.headers.get('Cookie'))
+}
+
+const getUserId = async (request:Request) => {
+    const session = await getUserSession(request)
+    const userId = session.get('userId')
+    if (!userId || typeof userId !== 'string') return null
+    return userId
+}
+
+
+export const getUser = async (request: Request) => {
+    const userId = await getUserId(request)
+    if (typeof userId !== 'string') {
+      return null
+    }
+  
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+        select: { id: true, email: true, profile: true },
+      })
+      return user
+    } catch {
+      throw logout(request)
+    }
+  }
+  
+  export const logout = async (request: Request) => {
+    const session = await getUserSession(request)
+    return redirect('/login', {
+      headers: {
+        'Set-Cookie': await storage.destroySession(session),
+      },
+    })
+  }
