b50cee0fd4a21bedd1828b10ca17b97ebf8283a9
Tim Kozemzak <timkozemzak@gmail.com>
2022-06-07 21:16:18 -0700

    adds user circle component

--- app/components/user-panel.tsx
+++ app/components/user-panel.tsx
@@ -1,4 +1,6 @@
 import { User } from '@prisma/client'
+import UserCircle from './user-circle'
+
 const UserPanel = ({ users }: { users: User[] }) => {
     console.log("users in userPanel:", users)
     return (
@@ -7,15 +9,19 @@ const UserPanel = ({ users }: { users: User[] }) => {
                 <h2 className="text-xl text-blue-600 font-semibold">My Team</h2>
             </div>
             <div className="flex-1 overflow-y-scroll py-4 flex flex-col gap-y-10">
-                <p>Users go here</p>
+                {users.map((user) => {
+                    return <UserCircle key={user.id} profile={user.profile} className="h-24 w-24 mx-auto flex-shrink-0" />
+                })}
             </div>
             <div className="text-center p-6 bg-gray-300">
-                <button
-                    type="submit"
-                    className="rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
-                >
-                    Sign Out
-                </button>
+                <form action="/logout" method="POST">
+                    <button
+                        type="submit"
+                        className="rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+                    >
+                        Sign Out
+                    </button>
+                </form>
             </div>
         </div>
     )
